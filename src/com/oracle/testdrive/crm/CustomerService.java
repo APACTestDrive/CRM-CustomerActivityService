35deb9c2b182ce7587857385029785078f9a44cb
Chris Choi <choichi@gmail.com>
2017-06-30 15:46:36 +0800

    Initial dummy CRM CustomerActivityService build

---
+++ src/com/oracle/testdrive/crm/CustomerService.java
@@ -0,0 +1,40 @@
+package com.oracle.testdrive.crm;
+
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import javax.xml.ws.BindingType;
+import javax.xml.ws.soap.SOAPBinding;
+
+
+@SuppressWarnings("oracle.jdeveloper.webservice.wsdl-impl-synchronization")
+@WebService(name = "CustomerService", wsdlLocation = "WEB-INF/wsdl/CustomerServiceService.wsdl")
+public class CustomerService {
+    public CustomerService() {}
+
+    @WebMethod
+    public String addCustomerActivity(@WebParam(name = "customerId") int customerId,
+                                      @WebParam(name = "activityName") String activityName,
+                                      @WebParam(name = "activityDetail") String activityDetail,
+                                      @WebParam(name = "activityDate") String activityDate) {
+        int randomNum;
+        int min = 10000;
+        int max = 99999;
+        String activityId = "";
+        System.out.println("customerId is: " + customerId);
+        System.out.println("activityName is: " + activityName);
+        System.out.println("activityDetail is: " + activityDetail);
+        System.out.println("activityDate is: " + activityDate);
+        if (activityName != "" && activityName != null) {
+            randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+            activityId = String.valueOf(customerId) + "-" + String.valueOf(randomNum) + "-v1.0";
+            System.out.println("activityId is: " + activityId);
+        }
+        
+        return activityId;
+    }
+}
